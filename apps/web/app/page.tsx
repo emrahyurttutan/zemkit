8a5727838ce406174529a3705bbf79c9df2adb21
Emrah Yurttutan <emrahyurttutan@gmail.com>
2024-03-20 15:13:33 +0300

    package name change

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@zemkit/ui/card";
+import { Code } from "@zemkit/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@zemkit/ui/button";
 
 function Gradient({
   conic,
